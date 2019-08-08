f6adba74936c9e1a28fc5dc6de43850acd43dca8
rays <569364343@qq.com>
2019-08-09 00:01:00 +0800

    多台主机同步测试

--- 01_CreatList/source/main.c
+++ 01_CreatList/source/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+#include <stdint.h>
+
+struct node
+{
+	uint32_t value;
+	struct node *next;
+};
+struct node {
+
+};
+int32_t init_list(struct node *head)
+{
+	if (head != NULL)
+    {
+	    return -1;
+	}
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
-	printf("Hello\r\n");
+	printf("The list test start!\r\n");
+
 }
